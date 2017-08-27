46a7cbb7c5f43f39c3bb4a84107d45f7d79bbb9a
aaplmath <aaplmath@users.noreply.github.com>
2017-08-27 12:31:21 -0400

    Update Checkstyle directions

--- scripts/pages.ts
+++ scripts/pages.ts
@@ -51,7 +51,7 @@ const pages: Page[] = [
     'Install the Checkstyle plugin',
     'Checkstyle makes sure your code is clean and readable. To install it:<ul>'
       + '<li>Open Eclipse</li>'
-      + '<li>Drag the "GO TO WEBSITE" button onto the Eclipse icon in your Dock/Task Bar—DO NOT click it.</li>'
+      + '<li>Drag the "GO TO WEBSITE" button below onto the Eclipse icon in your Dock/Task Bar (not the Eclipse shortcut on your Desktop if you\'re on Windows)—DO NOT click it.</li>'
       + '<li>Go through the installation, accept the terms, and click "OK" on any warnings.</li>'
       + '</ul>',
     [new Button('Continue', 'git-client')],
